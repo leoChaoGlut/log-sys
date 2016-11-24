2d8b0b55673ca40d2d46f331cc154413962444f9
zhoufeng <625285133@qq.com>
2016-11-24 14:49:32 +0800

    调整目录

--- service/collector-node/src/main/java/cn/yunyichina/log/collertor/node/upload/FileZipUpload.java
+++ service/collector-node/src/main/java/cn/yunyichina/log/collertor/node/upload/FileZipUpload.java
@@ -1,6 +1,7 @@
-package cn.yunyichina.log.collertor.node.test;
+package cn.yunyichina.log.collertor.node.upload;
 
 
+import cn.yunyichina.log.collertor.node.constant.UrlConstant;
 import cn.yunyichina.log.common.util.Zip;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -14,29 +15,13 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by Jonven on 2016/11/21.
  */
-public class FileUploadClient {
+public class FileZipUpload {
 
-    private static String uploadRrl = "http://localhost:10500/file/upload";
-
-    public static void main(String args[]) {
-        try {
-            File[] files = new File[3];
-            files[0] = new File("E:\\testLog1\\2016\\11\\15\\14\\21\\201611151421.log");
-            files[1] = new File("E:\\testLog1\\2016\\11\\15\\14\\23\\201611151423.log");
-            files[2] = new File("E:\\testLog1\\2016\\11\\15\\14\\25\\201611151425.log");
-            uploadFile(files,"E:\\testLog.zip");
-        } catch (Exception ex) {
-            Logger.getLogger(FileUploadClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static void uploadFile(File[] files,String basePath) throws Exception {
+    public static void uploadFile(File[] files, String basePath) throws Exception {
 
         Zip.zip(files, basePath);
         File file = new File(basePath);
@@ -46,7 +31,7 @@ public class FileUploadClient {
         }
 
         HttpClient httpclient = HttpClients.createDefault();
-        HttpPost httppost = new HttpPost(uploadRrl);
+        HttpPost httppost = new HttpPost(UrlConstant.UPLAOD_URL);
 
         HttpEntity httpEntity = MultipartEntityBuilder.create()
                 .addBinaryBody("file", file).build();
