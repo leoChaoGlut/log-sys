3acc919902aed31343ebfd55c03d8f66af524133
leo <leoiscoding@163.com>
2016-11-28 20:02:03 +0800

    完成搜索节点增量添加索引,并持久化

--- service/searcher-node/src/main/java/cn/yunyichina/log/service/searcherNode/service/UploadService.java
+++ service/searcher-node/src/main/java/cn/yunyichina/log/service/searcherNode/service/UploadService.java
@@ -1,12 +1,21 @@
 package cn.yunyichina.log.service.searcherNode.service;
 
 import cn.yunyichina.log.common.util.ZipUtil;
+import cn.yunyichina.log.component.index.builder.imp.ContextIndexBuilder;
+import cn.yunyichina.log.component.index.builder.imp.KeyValueIndexBuilder;
+import cn.yunyichina.log.component.index.builder.imp.KeywordIndexBuilder;
+import cn.yunyichina.log.service.searcherNode.constant.IndexType;
+import cn.yunyichina.log.service.searcherNode.util.IndexManager;
+import com.google.common.base.Objects;
 import com.google.common.io.Files;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.*;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Jonven on 2016/11/28.
@@ -14,14 +23,24 @@ import java.io.File;
 @Service
 public class UploadService {
 
+    @Autowired
+    IndexManager indexManager;
+
     @Value("${constants.upload.logSuffix}")
     private String LOG_SUFFIX;
+
     @Value("${constants.upload.indexSuffix}")
     private String INDEX_SUFFIX;
+
+    @Value("${constants.upload.bakSuffix}")
+    private String BAK_SUFFIX;
+
     @Value("${constants.upload.zipIndex}")
     private String ZIP_SUFFIX;
+
     @Value("${constants.upload.logRootDir}")
     private String LOG_ROOT_DIR;
+
     @Value("${constants.upload.indexRootDir}")
     private String INDEX_ROOT_DIR;
 
@@ -33,45 +52,97 @@ public class UploadService {
         }
         String zipFilePath = dir + File.separator + file.getName() + ZIP_SUFFIX;
         Files.write(bytes, new File(zipFilePath));
-        rebuildLogDirStructure(zipFilePath);
+        ZipUtil.unzip(zipFilePath, LOG_ROOT_DIR);
+        rebuildLogDirStructureAndUpdateIndex();
     }
 
-    private void rebuildLogDirStructure(String zipFilePath) throws Exception {
-        ZipUtil.unzip(zipFilePath, LOG_ROOT_DIR);
+    private void rebuildLogDirStructureAndUpdateIndex() throws Exception {
         File[] logFiles = new File(LOG_ROOT_DIR).listFiles();
         if (logFiles != null) {
             for (File file : logFiles) {
                 if (file.isFile()) {
-                    if (file.getName().endsWith(LOG_SUFFIX)) {
-                        String copiedLogFilePath = LOG_ROOT_DIR + File.separator + file.getName().substring(0, 4) +
-                                File.separator + file.getName().substring(4, 6) +
-                                File.separator + file.getName().substring(6, 8) +
-                                File.separator + file.getName().substring(8, 10) +
-                                File.separator + file.getName().substring(10, 12) +
-                                File.separator + file.getName();
-
-                        File copiedLogFile = new File(copiedLogFilePath);
-                        if (copiedLogFile.exists()) {
+                    String fileName = file.getName();
+                    String fileSuffix = fileName.substring(fileName.lastIndexOf("."));
+                    fileName = fileName.substring(0, fileName.lastIndexOf("."));
+                    if (Objects.equal(fileSuffix, LOG_SUFFIX)) {
+                        String oldLogFilePath = buildOldLogFilePath(fileName, fileSuffix);
+                        File oldLogFile = new File(oldLogFilePath);
+                        if (oldLogFile.exists()) {
                             file.delete();
                         } else {
-                            Files.createParentDirs(copiedLogFile);
-                            Files.move(file, copiedLogFile);
+                            Files.createParentDirs(oldLogFile);
+                            Files.move(file, oldLogFile);
                         }
-                    } else if (file.getName().endsWith(INDEX_SUFFIX)) {
-                        File copiedIndexFile = new File(INDEX_ROOT_DIR + file.getName());
-//                        TODO 硬盘索引备份,硬盘索引增量(先备份,后替换,且必须保留上一个文件的备份),内存索引增量
-//                        if (copiedIndexFile.exists()) {
-//
-//                        } else {
-//                            Files.createParentDirs(copiedIndexFile);
-//                            Files.move(file, copiedIndexFile);
-//                        }
+                    } else if (Objects.equal(fileSuffix, INDEX_SUFFIX)) {
+                        ObjectInputStream ois = null;
+                        try {
+                            ois = new ObjectInputStream(new FileInputStream(file));
+                            switch (fileName) {
+                                case IndexType.CONTEXT:
+                                    Map<Long, ContextIndexBuilder.ContextInfo> contextIndexMap = (Map<Long, ContextIndexBuilder.ContextInfo>) ois.readObject();
+                                    indexManager.appendContextIndex(contextIndexMap);
+                                    backupAndReplace(fileName, indexManager.getContextIndexMap());
+                                    break;
+                                case IndexType.KEYWORD:
+                                    Map<String, Set<KeywordIndexBuilder.IndexInfo>> keywordIndexMap = (Map<String, Set<KeywordIndexBuilder.IndexInfo>>) ois.readObject();
+                                    indexManager.appendKeywordIndex(keywordIndexMap);
+                                    backupAndReplace(fileName, indexManager.getKeywordIndexMap());
+                                    break;
+                                case IndexType.KEY_VALUE:
+                                    Map<String, Map<String, Set<KeyValueIndexBuilder.IndexInfo>>> keyValueIndexMap = (Map<String, Map<String, Set<KeyValueIndexBuilder.IndexInfo>>>) ois.readObject();
+                                    indexManager.appendKeyValueIndex(keyValueIndexMap);
+                                    backupAndReplace(fileName, indexManager.getKeyValueIndexMap());
+                                    break;
+                                default:
+                                    throw new Exception("不支持的索引类型");
+                            }
+                        } finally {
+                            file.delete();
+                            if (ois != null) {
+                                ois.close();
+                            }
+                        }
                     }
                 } else {
 
                 }
             }
         }
+    }
+
+    private String buildOldLogFilePath(String fileName, String fileSuffix) {
+        return LOG_ROOT_DIR + File.separator + fileName.substring(0, 4) +
+                File.separator + fileName.substring(4, 6) +
+                File.separator + fileName.substring(6, 8) +
+                File.separator + fileName.substring(8, 10) +
+                File.separator + fileName.substring(10, 12) +
+                File.separator + fileName + fileSuffix;
+    }
+
+    private void backupAndReplace(String fileName, Object obj) throws IOException {
+        File oldIndexFile = new File(INDEX_ROOT_DIR + fileName + INDEX_SUFFIX);
+        if (oldIndexFile.exists()) {
+            File bakIndexFile = new File(INDEX_ROOT_DIR + fileName + BAK_SUFFIX);
+            if (bakIndexFile.exists()) {
+
+            } else {
+                Files.createParentDirs(bakIndexFile);
+            }
+            Files.write(Files.asByteSource(oldIndexFile).read(), bakIndexFile);
+        } else {
+            Files.createParentDirs(oldIndexFile);
+        }
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(oldIndexFile));
+            oos.writeObject(obj);
+            oos.flush();
+        } finally {
+            if (oos != null) {
+                oos.close();
+            }
+        }
+
     }
 
 }
--- service/searcher-node/src/main/java/cn/yunyichina/log/service/searcherNode/util/IndexManager.java
+++ service/searcher-node/src/main/java/cn/yunyichina/log/service/searcherNode/util/IndexManager.java
@@ -1,5 +1,8 @@
 package cn.yunyichina.log.service.searcherNode.util;
 
+import cn.yunyichina.log.component.aggregator.index.imp.ContextIndexAggregator;
+import cn.yunyichina.log.component.aggregator.index.imp.KeyValueIndexAggregator;
+import cn.yunyichina.log.component.aggregator.index.imp.KeywordIndexAggregator;
 import cn.yunyichina.log.component.index.builder.imp.ContextIndexBuilder;
 import cn.yunyichina.log.component.index.builder.imp.KeyValueIndexBuilder;
 import cn.yunyichina.log.component.index.builder.imp.KeywordIndexBuilder;
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,19 +32,55 @@ public class IndexManager {
 
     @PostConstruct
     public void init() {
-        buildContextIndex();
-        buildKeywordIndex();
-        buildKeyValueIndex();
+        appendContextIndex(null);
+        appendKeywordIndex(null);
+        appendKeyValueIndex(null);
     }
 
 
-    private void buildKeyValueIndex() {
+    public void appendKeyValueIndex(Map<String, Map<String, Set<KeyValueIndexBuilder.IndexInfo>>> keyValueIndexMap) {
+        if (this.keyValueIndexMap == null) {
+            if (keyValueIndexMap == null) {
+                this.keyValueIndexMap = new HashMap<>();
+            } else {
+                this.keyValueIndexMap = keyValueIndexMap;
+            }
+        } else {
+            KeyValueIndexAggregator aggregator = new KeyValueIndexAggregator();
+            aggregator.aggregate(this.keyValueIndexMap);
+            aggregator.aggregate(keyValueIndexMap);
+            this.keyValueIndexMap = aggregator.getAggregatedCollection();
+        }
     }
 
-    private void buildKeywordIndex() {
+    public void appendKeywordIndex(Map<String, Set<KeywordIndexBuilder.IndexInfo>> keywordIndexMap) {
+        if (this.keywordIndexMap == null) {
+            if (keywordIndexMap == null) {
+                this.keywordIndexMap = new HashMap<>();
+            } else {
+                this.keywordIndexMap = keywordIndexMap;
+            }
+        } else {
+            KeywordIndexAggregator aggregator = new KeywordIndexAggregator();
+            aggregator.aggregate(this.keywordIndexMap);
+            aggregator.aggregate(keywordIndexMap);
+            this.keywordIndexMap = aggregator.getAggregatedCollection();
+        }
     }
 
-    private void buildContextIndex() {
+    public void appendContextIndex(Map<Long, ContextIndexBuilder.ContextInfo> contextIndexMap) {
+        if (this.contextIndexMap == null) {
+            if (contextIndexMap == null) {
+                this.contextIndexMap = new HashMap<>();
+            } else {
+                this.contextIndexMap = contextIndexMap;
+            }
+        } else {
+            ContextIndexAggregator aggregator = new ContextIndexAggregator();
+            aggregator.aggregate(this.contextIndexMap);
+            aggregator.aggregate(contextIndexMap);
+            this.contextIndexMap = aggregator.getAggregatedCollection();
+        }
     }
 
     public Map<Long, ContextIndexBuilder.ContextInfo> getContextIndexMap() {
