4091225a2c720bbba733e82a48834f2112941758
leo <leoiscoding@163.com>
2016-12-08 14:35:07 +0800

    抽取各个服务的变化配置到配置中心

--- service/searcher-gateway/src/main/java/cn/yunyichina/log/service/searcherGateway/controller/DispatchController.java
+++ service/searcher-gateway/src/main/java/cn/yunyichina/log/service/searcherGateway/controller/DispatchController.java
@@ -1,12 +1,21 @@
 package cn.yunyichina.log.service.searcherGateway.controller;
 
 import cn.yunyichina.log.common.entity.dto.Response;
+import cn.yunyichina.log.common.entity.dto.SearchCondition;
 import cn.yunyichina.log.common.log.LoggerWrapper;
 import cn.yunyichina.log.service.searcherGateway.service.DispatchService;
+import com.alibaba.fastjson.JSON;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author: Leo
@@ -26,15 +35,32 @@ public class DispatchController {
     public Response dispatch(@RequestBody String jsonParam) {
         try {
             logger.info("搜索网关接收到请求:" + jsonParam);
-//            TODO 正确代码
-//            SearchCondition condition = JSON.parseObject(jsonParam, SearchCondition.class);
-//            Response response = dispatchService.dispatch(condition);
-//            return response;
-            return Response.success("搜索网关成功");
+            SearchCondition condition = JSON.parseObject(jsonParam, SearchCondition.class);
+            Response response = dispatchService.dispatch(condition);
+            return response;
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage(), e);
             return Response.failure(e.getLocalizedMessage());
         }
+    }
+
+    @Value("${server.port}")
+    int serverPort;
+
+    @Autowired
+    EurekaClient eurekaClient;
+
+    @GetMapping("test")
+    public String test() {
+        Application application = eurekaClient.getApplication("searcher-gateway");
+        List<InstanceInfo> instanceInfoList = application.getInstances();
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (InstanceInfo ii : instanceInfoList) {
+            sb.append("{" + ii.getHostName() + "," + ii.getIPAddr() + "," + ii.getPort() + "}");
+        }
+        sb.append("]");
+        return serverPort + sb.toString();
     }
 
 }
