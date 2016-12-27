1f883dddd606c66713a1507b66de77f6bd498062
leo <leoiscoding@163.com>
2016-12-27 11:32:00 +0800

    修改gitignore忽略掉正确的文件的问题

---
+++ common/src/main/java/cn/yunyichina/log/common/constant/IndexType.java
@@ -0,0 +1,14 @@
+package cn.yunyichina.log.common.constant;
+
+/**
+ * @Author: Leo
+ * @Blog: http://blog.csdn.net/lc0817
+ * @CreateTime: 2016/11/28 18:34
+ * @Description:
+ */
+public interface IndexType {
+
+    String CONTEXT = "context";
+    String KEYWORD = "keyword";
+    String KEY_VALUE = "keyValue";
+}
