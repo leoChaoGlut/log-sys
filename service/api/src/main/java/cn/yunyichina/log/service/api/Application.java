98dccfb93e31525546563a1da261d8c845c88be5
leo <leoiscoding@163.com>
2017-01-03 16:02:52 +0800

    添加redis

---
+++ service/api/src/main/java/cn/yunyichina/log/service/api/Application.java
@@ -0,0 +1,23 @@
+package cn.yunyichina.log.service.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @Author: Leo
+ * @Blog: http://blog.csdn.net/lc0817
+ * @CreateTime: 2017/1/3 16:00
+ * @Description:
+ */
+@EnableWebMvc
+@SpringBootApplication(scanBasePackages = "cn.yunyichina.log.service.api")
+@EnableEurekaClient
+@EnableMongoRepositories(basePackages = "cn.yunyichina.log.service.api.repository")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
