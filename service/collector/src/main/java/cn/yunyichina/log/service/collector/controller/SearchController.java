74d70ea1bccdef1ef34f6da487b9a54f8b7494ae
leo <leoiscoding@163.com>
2017-01-05 16:58:26 +0800

    update

--- service/collector/src/main/java/cn/yunyichina/log/service/collector/controller/SearchController.java
+++ service/collector/src/main/java/cn/yunyichina/log/service/collector/controller/SearchController.java
@@ -1,5 +1,6 @@
 package cn.yunyichina.log.service.collector.controller;
 
+import cn.yunyichina.log.common.log.LoggerWrapper;
 import cn.yunyichina.log.component.entity.dto.Response;
 import cn.yunyichina.log.component.entity.dto.SearchCondition;
 import cn.yunyichina.log.service.collector.service.SearchService;
@@ -22,16 +23,21 @@ import java.util.List;
 @RequestMapping("search")
 public class SearchController {
 
+    final LoggerWrapper logger = LoggerWrapper.getLogger(SearchController.class);
+
     @Autowired
     SearchService searchService;
 
     @PostMapping("realtime")
     public Response realtime(@RequestBody String jsonParam) {
         try {
+            logger.contextBegin("实时日志搜索开始:" + jsonParam);
             SearchCondition condition = JSON.parseObject(jsonParam, SearchCondition.class);
             List<String> contextList = searchService.realtime(condition);
+            logger.contextEnd("实时日志搜索结束:" + JSON.toJSONString(contextList, true));
             return Response.success(contextList);
         } catch (Exception e) {
+            logger.contextEnd("实时日志搜索失败:" + e.getLocalizedMessage());
             return Response.failure(e.getLocalizedMessage());
         }
     }
