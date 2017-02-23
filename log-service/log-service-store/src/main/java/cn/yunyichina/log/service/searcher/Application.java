bda7f9bdad600d9bbb4352c00ce795e49ecd900a
leo <leoiscoding@163.com>
2017-02-23 12:33:28 +0800

    change httpmsgconverter from jackson to fastjson

--- log-service/log-service-store/src/main/java/cn/yunyichina/log/service/searcher/Application.java
+++ log-service/log-service-store/src/main/java/cn/yunyichina/log/service/searcher/Application.java
@@ -1,6 +1,9 @@
 package cn.yunyichina.log.service.searcher;
 
 import cn.yunyichina.log.service.searcher.constants.CacheName;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
+import com.google.common.base.Charsets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
@@ -8,12 +11,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -26,7 +32,24 @@ import java.util.List;
 @EnableEurekaClient
 @EnableCaching
 @SpringBootApplication(scanBasePackages = "cn.yunyichina.log.service.searcher")
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter4 fastJsonConverter = new FastJsonHttpMessageConverter4();
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setCharset(Charsets.UTF_8);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConverter.setFastJsonConfig(fastJsonConfig);
+
+        fastJsonConverter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.APPLICATION_JSON_UTF8
+        ));
+
+        converters.add(fastJsonConverter);
+    }
+
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
